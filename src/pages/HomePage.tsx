d06c0afd31ae00f84ab6ab2fb35979f4e8a8c81f
JYCHO <cjy0029@naver.com>
2022-02-28 01:53:44 +0900

    feat : Home screen 생성 및 추가

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+};
+
+export default HomePage;
+
+// const styles = StyleSheet.create({});
